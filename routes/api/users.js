7b67af5994f421a78acecdf7d32e0f9cc9f775d2
toleen98 <toleenballool98@gmail.com>
2020-10-09 15:35:26 +0300

    user routes/ create login route with passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,7 @@ const keys = require("../../config/keys");
 const User = require("../../models/User");
 
 router.post("/register", (req, res) => {
-    
+
   User.findOne({ email: req.body.email }).then(user => {
       if (user) {
         return res.status(400).json({ email: "Email already exists" });
@@ -32,3 +32,48 @@ router.post("/register", (req, res) => {
       }
     });
   });
+
+  //Login route
+
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+      const password = req.body.password;
+    // Find user by email
+      User.findOne({ email }).then(user => {
+        // Check if user exists
+        if (!user) {
+          return res.status(404).json({ emailnotfound: "Email not found" });
+        }
+    // Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+          if (isMatch) {
+            // User matched
+            // Create JWT Payload
+            const payload = {
+              id: user.id,
+              name: user.name
+            };
+    // Sign token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {
+                expiresIn: 31556926 // 1 year in seconds
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              }
+            );
+          } else {
+            return res
+              .status(400)
+              .json({ passwordincorrect: "Password incorrect" });
+          }
+        });
+      });
+});
+
+module.exports = router;
