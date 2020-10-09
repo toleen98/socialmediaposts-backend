24a8aad0cf42c0b01662ff1f0408e49129903b1a
toleen98 <toleenballool98@gmail.com>
2020-10-09 15:00:24 +0300

    user routes/ create registration route to add user to db

---
+++ routes/api/users.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+// Load User model
+const User = require("../../models/User");
+
+router.post("/register", (req, res) => {
+    
+  User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+  // Hash password before saving in database
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  });
